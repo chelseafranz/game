ec54ff6ab404cc18f52e311928edbdeed343c614
Chelsea Franz <chelseafranz3@gmail.com>
2014-10-23 15:47:13 -0400

    names on page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,114 +1,69 @@
 var Player = function(options){
   this.name=options.name;
   this.health=options.health || 100;
-  this.age = options. age || {};
+  switch(this.type){
+    case 'a':
+      attack_points= 10;
+      special_points=20;
+      break;
+    case 'b':
+      attack_points= 20;
+      special_points=40;
+      break;
+    case 'c':
+      attack_points= 5;
+      special_points=30;
+      break;
+  }
 
+  this.health = function(opponent){
+    opponent.health = opponent.health - (_.random(0,15));
+  };
+  this.bigattack = function(){
+    
+  };
 };
 
-Player.prototype.littleattack= function(monster){
-monster.health = monster.health - (_.random(0, 12));
-};
-Player.prototype.bigattack= function(monster){
-monster.health = monster.health - (_.random(20, 50));
-};
-
-
-var Monster = function(options){
+var Villan = function(options){
   this.name= options.name;
-  this.health= options.health || 100;
-  this.age= options.age || {};
+  this.health= function(opponent){
+    opponent.health = opponent.health - (_.random(0,15));
+  };
+  this.bigattack = function(){
+
+  };
+  this.type = options.type;
 };
 
-Monster.prototype.counterattack= function(person){
-person.health = person.health - (_.random(0, 50));
-};
 
-var ben = new Player ({
-  name: 'Ben',
-  age: 34,
+//when any button inside of '.start' is clicked..
+$('.start button').on('click', function(event){
+  event.preventDefault();
+//take the 'name' of whichever button is clicked, 
+//assign that button's text value to be equal to 'new Player'
+var character_type=$(this).attr('name'),
+      character_name= $(this).text();
+
+var player = new Player({
+  name: character_name,
+  type: character_type
 });
-
-var jerry = new Monster({
-  name: 'Jerry',
-});
-
-var samson = new Player({
-  name: 'Samson'
-});
-
-var delilah = new Monster({
-  name: 'Delilah'
+$('.start').fadeOut();
+$('.go').text(player.name);
 });
 
 
-$('h3').click(function(){
-  $('h3').fadeToggle("slow", "linear");
-  $('h1').addClass('center');
+$('.start2 button').on('click', function(event){
+  event.preventDefault();
+
+var character_type=$(this).attr('name'),
+      character_name= $(this).text();
+
+var villan = new Villan({
+  name: character_name,
+  type: character_type
 });
-
-
-$('.head').one('click','.center',function(){
-$('.player1').append('<h4 class="choose">Choose your players </h4>');
-$('.player1').append('<button class="p1">player one</button>');
-$('.player1').append('<button class="p2">player two</button>');
-});
-
-//choose your players//////////////////////////////
-$('.player1').one('click','.p1',function(){
-  $('ul').removeClass('hide');
-  $('ul').addClass('show');
-});
-
-$('.player1').one('click','.p2',function(){
-  $('ol').removeClass('hide2');
-  $('ol').addClass('show2');
-});
-
-$('ul').one('click', '.select', function(){
-  $('.left').append('<form><input type="checkbox">I am ready to play</form>');
-});
-
-$('ol').one('click',function(){
-$('.right').append('<form><input class="fp" type="checkbox">I am even more ready to play</form>');
-
-$( "input" ).on( 'click',function(){
-$('.choose').hide("slow", "linear");
-$('ul').fadeToggle("slow", "linear");
-$('ol').fadeToggle("slow", "linear");
-$('form').fadeToggle("slow", "linear");
-});
-$('.game').removeClass('gone')
-$('.game').addClass('see');
-});
-
-// $('.game').on('click', function(){
-//   $('.game').removeClass('gone')
-//   $('.game').addClass('see');
-// });
-
-// attacks/////////////////////////////////
-
-$('.gameL').on('click', '.a',function(){
-  $('.gameL').append('<div>lets go</div>');
-});
-
-var playerOne;
-var playerTwo;
-
-$('.a').on('click', function(){
-  playerOne= ben;
-
-  $('.d').on('click', function(){
-    playerTwo= jerry;
-
-    $('.pow').on('click', function(){
-      playerOne.bigattack(playerTwo);
-      playerTwo.bigattack(playerOne);
-      $('.healthScore').html(playerTwo.health);
-
-});
-
-});
-
-
+$('.start2').hide("slow");
+$('.play').show("slow");
+$('.go2').text(villan.name);
 });
